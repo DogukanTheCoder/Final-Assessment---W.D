b4e892e0cca78925a908fd349a0c8245466f8a92
DOGUKAN AKTAS <d.aktas.22@abdn.ac.uk>
2022-12-04 22:44:34 +0000

    Added more information into the home page and added extra code for the chat page to display active users and made sure the navigation bars were functional

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const socket = require("socket.io");
 
-// App setup
 const PORT = 5000;
 const app = express();
 const server = app.listen(PORT, function () {
@@ -9,13 +8,11 @@ const server = app.listen(PORT, function () {
 
 });
 
-// Static files
 app.use(express.static("public"));
 
-// Socket setup
 const io = socket(server);
 
-//we use a set to store users, sets objects are for unique values of any type
+
 const activeUsers = new Set();
 
 io.on("connection", function (socket) {
@@ -24,7 +21,7 @@ io.on("connection", function (socket) {
   socket.on("new user", function (data) {
     socket.userId = data;
     activeUsers.add(data);
-    //... is the the spread operator, adds to the set while retaining what was in there already
+    
     io.emit("new user", [...activeUsers]);
   });
 
